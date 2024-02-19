467d5a9f52213e1a6a8de09a2ff0bccec50d0661
D3r3-k <d3r3k16kd@gmail.com>
2024-02-18 22:47:19 -0600

    agregando funcion graficar y arreglando funciones

--- Graficar.py
+++ Graficar.py
@@ -7,38 +7,15 @@ def graficar():
         archivoDOT = open(archivo, "w")
         archivoDOT.write("digraph{\n")
         archivoDOT.write("rankdir = LR;\n")
-        archivoDOT.write(
-            'node [fontname = "Arial Black"; fontsize = 14; margin = "0.2,0.1";];\n')
+        archivoDOT.write('node [fontname = "Arial Black"; fontsize = 14; margin = "0.2,0.1";];\n')
         archivoDOT.write('edge [color = "#008cff";];\n')
-
-        listaActores = []
-        for pelicula in lista_peliculas:
-            nodo = ('"'+pelicula.titulo+'"[shape = record;color = red;style = diagonals;label ="' +
-                    pelicula.titulo+'| {'+pelicula.anio+' | '+pelicula.genero+'}";margin = "0.3,0.2";fontsize = 10;];\n')
-            archivoDOT.write(nodo)
-
-            for actor in pelicula.actores:
-                if actor not in listaActores:
-                    listaActores.append(actor)
-
-        for actor in listaActores:
-            nodo = ('"'+actor+'"[shape = box;color = purple;label ="' +
-                    actor+'";margin = "0.3,0.2";fontsize = 10;];\n')
-            archivoDOT.write(nodo)
-
-        for pelicula in lista_peliculas:
-            for actorP in pelicula.actores:
-                for actorL in listaActores:
-                    if actorP == actorL:
-                        nodo = ('"'+pelicula.titulo+'"'+'->'+'"'+actorP+'"\n')
-                        archivoDOT.write(nodo)
         archivoDOT.write("}\n")
         archivoDOT.close()
         os.system("dot.exe -Tpng "+archivo+" -o " +
                   archivo.replace(".dot", ".png"))
 
-        print("     [✓] Renderizacion completa!")
+        print("     [✓] Gráfica creada!")
         input("     Presiona una tecla para continuar...")
     except:
-        input("\n     [X] Hubo un error al graficar!"
+        input("\n     [X] Hubo un error al crear la gráfica!"
               "\n     Presiona una tecla para continuar...")
